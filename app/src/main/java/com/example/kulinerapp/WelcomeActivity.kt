b1ecdc5b7d6de6a09afb2c1949fb50fa105bfe3f
tsabitfarel <tsabitzfarel@gmail.com>
2020-11-13 08:21:07 +0800

    init

---
+++ app/src/main/java/com/example/kulinerapp/WelcomeActivity.kt
@@ -0,0 +1,18 @@
+package com.example.kulinerapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        btn_welcome.setOnClickListener {
+            val intent = Intent(this, makananActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
